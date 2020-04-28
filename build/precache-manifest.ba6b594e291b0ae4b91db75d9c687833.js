2d642f68fbad7967489d862fff5c37b232146c45
GAlekhya123 <alekhyaganji440@gmail.com>
2020-04-28 15:46:23 +0530

    profileversion1

---
+++ build/precache-manifest.ba6b594e291b0ae4b91db75d9c687833.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f71713d6792cf840cb64c2453d391f1",
+    "url": "/profilereact1/index.html"
+  },
+  {
+    "revision": "2c63429458ba3beee57e",
+    "url": "/profilereact1/static/css/2.b6a8c48d.chunk.css"
+  },
+  {
+    "revision": "14181e53dccf679b5c81",
+    "url": "/profilereact1/static/css/main.a3b3e5c1.chunk.css"
+  },
+  {
+    "revision": "2c63429458ba3beee57e",
+    "url": "/profilereact1/static/js/2.acd32ed3.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/profilereact1/static/js/2.acd32ed3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14181e53dccf679b5c81",
+    "url": "/profilereact1/static/js/main.8b3269cf.chunk.js"
+  },
+  {
+    "revision": "2a326148f4a6b9fe8b5b",
+    "url": "/profilereact1/static/js/runtime-main.efb2372c.js"
+  }
+]);
